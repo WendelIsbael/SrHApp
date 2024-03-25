f55d17ff4577ed0f3a8d70a5f5dbf841e6b67a24
WENDEL SILVA DE JESUS <wendel.sjesus@senacsp.edu.br>
2024-03-25 14:58:22 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Sr. Health/Download</title>
+    <style>
+        #btn{
+            background-color: #F6F4EB;
+            color: #91C8E4;
+            transition: all 0,6;
+        }
+
+        #btn:hover{
+            background-color: #4682A9;
+            color: #F6F4EB;
+            box-shadow: 0px 3px 12px rgba(0, 0, 0, 0.10)
+        }
+    </style>
+</head>
+<body class="bg-primary-subtle">
+
+    <div class="container text-center text-light">
+        <div class="row justify-content-center align-items-center vh-100">
+        <div class="col ">
+            <h1 class="text-info-emphasis"><img src="logo.png" alt="logo Sr.Health">Sr Health</h1>
+            <div class="container p-3 px-4 rounded-3 bg-primary bg-opacity-50 shadow "style="width:360px; backdrop-filter: blur(4px)">
+                <h2>Beta Teste</h2>
+                <div class="text-start">
+                    <p class="Text-start">Experimente a versão beta do nosso aplicativo médico! Teste prontuários digitais entre equipes médicas antes do lançamento oficial. Ajude-nos a aprimorar a prestação de cuidados. Disponível para download em dispositivos Android.</p>
+                </div>
+                <a href="app-release 8.apk" class="btn  px-4" id="btn"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg>      v0.5</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
